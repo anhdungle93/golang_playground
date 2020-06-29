101c2232b95a1db0282555cdddf7277f9daa1c31
anhdungle <anhdungle634@gmail.com>
2020-06-29 15:55:37 +0200

    channels example

---
+++ src/channels_examples.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	ch := make(chan int)
+	wg.Add(2)
+	go func(ch <-chan int) {
+		i := <-ch
+		fmt.Println(i)
+		wg.Done()
+	}(ch)
+	go func(ch chan<- int) {
+		ch <- 42
+		wg.Done()
+	}(ch)
+	wg.Wait()
+}
+
+/* func main() {
+	ch := make(chan int)
+	counter := 0
+	for j := 0; j < 5; j++ {
+		wg.Add(2)
+		go func() {
+			i := <-ch
+			fmt.Println(i)
+			wg.Done()
+		}()
+		go func() {
+			ch <- counter
+			counter++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+} */
+
+/* func main() {
+	ch := make(chan int)
+	wg.Add(2)
+	go func() {
+		i := <-ch
+		fmt.Println(i)
+		wg.Done()
+	}()
+	go func() {
+		ch <- 42
+		wg.Done()
+	}()
+	wg.Wait()
+} */
